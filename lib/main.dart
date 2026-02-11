6ab5a2b9c15591d5eb8b97ce8bf6ea5f0c9f264d
a.23308051280544 <a.23308051280544@cbtis128.edu.mx>
2026-02-11 20:33:21 +0000

    otra vez

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,130 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(AppPetco());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class AppPetco extends StatelessWidget {
+  const AppPetco({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return MaterialApp(debugShowCheckedModeBanner: false, home: LasMascotas());
+  }
+} // fin clase AppPetco
+
+class LasMascotas extends StatelessWidget {
+  const LasMascotas({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            // Color de fondo azul claro
+            backgroundColor: const Color.fromARGB(255, 139, 2, 37),
+            centerTitle: true,
+            title: const Text(
+              "Petco Contreras",
+              style: TextStyle(
+                // Color de texto guinda oscuro
+                color: Color.fromARGB(255, 122, 183, 253), 
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+            // Icono de mascota a la izquierda
+            leading: const Icon(
+              Icons.pets,
+              color: Color.fromARGB(255, 122, 183, 253), 
+            ),
+            // Acciones a la derecha
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.inventory_2), // Caja
+                color: const Color.fromARGB(255, 122, 183, 253), 
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: const Icon(Icons.sell), // Vender / Etiqueta
+                color: const Color.fromARGB(255, 122, 183, 253), 
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: const Icon(Icons.medical_services), // Medicina
+                color: const Color.fromARGB(255, 122, 183, 253), 
+                onPressed: () {},
+              ),
+            ],
+          ),
+      body:  SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    // TARJETA 1
+                    Card(
+                      color: const Color(0xFFFFB3B3), // Rojo pastel
+                      elevation: 5,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                      child: const ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: NetworkImage('https://raw.githubusercontent.com/RolandoLimones/imagenes-para-flutter-6toI-11-feb-2026/refs/heads/main/mascota1.png'),
+                        ),
+                        title: Text("Rolando Contreras", style: TextStyle(color: Color(0xFF0047AB), fontWeight: FontWeight.bold)),
+                        subtitle: Text("Perro labrador - 2 años", style: TextStyle(color: Color(0xFF0047AB))),
+                        trailing: Icon(Icons.favorite, color: Color(0xFF0047AB)),
+                      ),
+                    ),
+                    const SizedBox(height: 10), // Espacio entre tarjetas
+
+                    // TARJETA 2
+                    Card(
+                      color: const Color(0xFFFFB3B3),
+                      elevation: 5,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                      child: const ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: NetworkImage('https://raw.githubusercontent.com/RolandoLimones/imagenes-para-flutter-6toI-11-feb-2026/refs/heads/main/mascota2.png'),
+                        ),
+                        title: Text("Michi", style: TextStyle(color: Color(0xFF0047AB), fontWeight: FontWeight.bold)),
+                        subtitle: Text("Perro - 1 año", style: TextStyle(color: Color(0xFF0047AB))),
+                        trailing: Icon(Icons.favorite, color: Color(0xFF0047AB)),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+
+                    // TARJETA 3
+                    Card(
+                      color: const Color(0xFFFFB3B3),
+                      elevation: 5,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                      child: const ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: NetworkImage('https://raw.githubusercontent.com/RolandoLimones/imagenes-para-flutter-6toI-11-feb-2026/refs/heads/main/mascota3.png'),
+                        ),
+                        title: Text("Bugs", style: TextStyle(color: Color(0xFF0047AB), fontWeight: FontWeight.bold)),
+                        subtitle: Text("Gato Blanco - 6 meses", style: TextStyle(color: Color(0xFF0047AB))),
+                        trailing: Icon(Icons.favorite, color: Color(0xFF0047AB)),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+
+                    // TARJETA 4
+                    Card(
+                      color: const Color(0xFFFFB3B3),
+                      elevation: 5,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                      child: const ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: NetworkImage('https://raw.githubusercontent.com/RolandoLimones/imagenes-para-flutter-6toI-11-feb-2026/refs/heads/main/mascota4.jpg'),
+                        ),
+                        title: Text("Coco", style: TextStyle(color: Color(0xFF0047AB), fontWeight: FontWeight.bold)),
+                        subtitle: Text("Gato negro - 3 años", style: TextStyle(color: Color(0xFF0047AB))),
+                        trailing: Icon(Icons.favorite, color: Color(0xFF0047AB)),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),  
+
+    );// fin de scaffold
   }
-}
+}// fin clase LosAnimales
